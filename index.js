26b1bd4b41420fa767a5936dbd8e4c7882f9698e
Javier Pacheco <111820024+JAPACX@users.noreply.github.com>
2024-04-29 11:35:51 -0500

    feature: add condif to database conection for silent mode

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import "dotenv/config";
 import express from "express";
-import { promises as fs } from "fs";
 import { handler } from "./src/index.js";
 
 const app = express();
 const PORT = 3000;
 
-app.get("/invoke", (req, res) => {
-  handler({})
+app.get("/", (req, res) => {
+  handler({}, {})
     .then((response) => {
       res.json(JSON.parse(response.body));
     })
@@ -17,26 +16,4 @@ app.get("/invoke", (req, res) => {
     });
 });
 
-app.get("/invoke/:eventName", (req, res) => {
-  const { eventName } = req.params;
-  const filePath = `./events/${eventName}.json`;
 
-  fs.readFile(filePath, "utf8")
-    .then((data) => {
-      const event = JSON.parse(data);
-      return handler(event);
-    })
-    .then((response) => {
-      res.json(JSON.parse(response.body));
-    })
-    .catch((err) => {
-      console.error(
-        `Error reading the event file or executing Lambda with event: ${err.message}`
-      );
-      res.status(500).send("Event not found or error processing the event");
-    });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
