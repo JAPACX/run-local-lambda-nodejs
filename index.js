f11b3a3a1656d513b6a58c741cf3e55cf7752368
Javier Pacheco <111820024+JAPACX@users.noreply.github.com>
2024-05-06 10:58:57 -0500

    fix: change to response replace promises

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ import { handler } from "./src/index.mjs";
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  handler({}, {})
-    .then((response) => {
-      res.json(JSON.parse(response.body));
-    })
-    .catch((err) => {
-      console.error("Error executing Lambda without an event:", err);
-      res.status(500).send("Error processing request without event");
-    });
+app.get("/", async (req, res) => {
+  try {
+    const response = await handler({}, {});
+    res.json(JSON.parse(response.body));
+  } catch (err) {
+    console.error("Error executing Lambda without an event:", err);
+    res.status(500).send("Error processing request without event");
+  }
 });
 
 app.listen(PORT, () => {
