065aaef52ce99753aaed4fb75b82aa33d638b27d
jepachecoam <javierpacheco@bemaster.com>
2024-06-12 14:27:24 -0500

    first commit

---
+++ src/utils/templates.mjs
@@ -0,0 +1,14 @@
+const templates = [
+    {
+        subjectKeywords: ["Información de su envío"],
+        textKeywords: ["presenta inconsistencias y fue rechazada por nuestra área operativa"],
+        code: 1
+    },
+    {
+        subjectKeywords: ["TCC Solucion Novedades"],
+        textKeywords: ["Le confirmamos que ha solucionado la novedad de la remesa"],
+        code: 2
+    }
+];
+
+export default { templates };
---
+++ src/utils/utils.mjs
@@ -0,0 +1,44 @@
+import t from "./templates.mjs";
+
+const extractShipmentCode = (subject, text) => {
+    const regex = /\b\d{9}\b/g;
+    const subjectMatch = subject.match(regex);
+    const textMatch = text.match(regex);
+
+    if (subjectMatch) {
+        return subjectMatch[0];
+    } else if (textMatch) {
+        return textMatch[0];
+    }
+
+    return null;
+};
+
+const normalizeText = (text) => {
+    return text.normalize("NFD")  // Normalize characters to NFD, splitting the character and the diacritic mark
+        .replace(/[\u0300-\u036f]/g, "")  // Remove all diacritic marks
+        .replace(/\s+/g, ' ')  // Replace all whitespace with single spaces
+        .trim()  // Trim the resulting string
+        .toLowerCase();  // Convert to lower case
+};
+
+const validateEmailContent = (subject, text) => {
+    let result = { isValid: false, code: null, shipmentCode: null };
+    const normalizedSubject = normalizeText(subject);
+    const normalizedText = normalizeText(text);
+
+    for (const template of t.templates) {
+        if (template.subjectKeywords.every(keyword => normalizedSubject.includes(normalizeText(keyword))) &&
+            template.textKeywords.every(keyword => normalizedText.includes(normalizeText(keyword)))) {
+            result.isValid = true;
+            result.code = template.code;
+            result.shipmentCode = extractShipmentCode(subject, text);
+            return result;
+        }
+    }
+
+    result.shipmentCode = extractShipmentCode(subject, text);
+    return result;
+};
+
+export default { validateEmailContent };
